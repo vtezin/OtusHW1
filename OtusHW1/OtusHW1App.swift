f11fab4886576320df9eb14d89335cded29545a3
Vladimir Tezin <vtezin@gmail.com>
2023-09-06 10:12:16 +0300

    Initial Commit

---
+++ OtusHW1/OtusHW1App.swift
@@ -0,0 +1,17 @@
+//
+//  OtusHW1App.swift
+//  OtusHW1
+//
+//  Created by user on 06.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct OtusHW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
